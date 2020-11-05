df7da4732b23f32c4101595b36b421c020964b7a
KaanKara <41960384+karaKaan@users.noreply.github.com>
2020-11-05 12:46:45 +0100

    the url now changes with the User input

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,13 +1,18 @@
-import React, {} from 'react'
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
+
 import './Home.scss'
 
 
 export default function Home() {
+    const [userInput, setUserInput] = useState("")
+
+console.log(userInput);
     return(
         <div>
             <form action="" className="formWrapper">
-                <input type="text" value="dasdasd"/>
-                <button>Search</button>
+                <input type="text" placeholder="Search your Amazon Product" onChange={e => {setUserInput(e.target.value)}}/>
+                <button><Link to={`/product/${userInput}`}>Search</Link></button>
 
             </form>
         </div>
